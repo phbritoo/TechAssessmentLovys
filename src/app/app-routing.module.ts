9f5544f2de86c0a83a938c14db7da4272b10207b
Paulo H. Brito de Santanta <paulo.santana@southsystem.com.br>
2022-02-10 00:57:54 -0300

    Create routing and settings modules App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { MoviePopularComponent } from './view/movie-popular/movie-popular.component';
+import { MovieTrendingComponent } from './view/movie-trending/movie-trending.component';
+import { SingupComponent } from './view/singup/singup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home/:page', component: HomeComponent },
+  { path: 'trending/:page', component: MovieTrendingComponent },
+  { path: 'latest/:page', component: MoviePopularComponent },
+  { path: 'singup', component: SingupComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './view/home/home.component';
+import { MovieNavBarComponent } from './view/movie-nav-bar/movie-nav-bar.component';
+import { MoviePopularComponent } from './view/movie-popular/movie-popular.component';
+import { MovieSearchComponent } from './view/movie-search/movie-search.component';
+import { MovieTrendingComponent } from './view/movie-trending/movie-trending.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment.prod';
+import { FirebaseService } from './service/firebase.service';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { SingupComponent } from './view/singup/singup.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieNavBarComponent,
+    MoviePopularComponent,
+    MovieSearchComponent,
+    MovieTrendingComponent,
+    SingupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MDBBootstrapModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
