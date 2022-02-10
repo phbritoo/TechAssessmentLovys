cc8db20eb24f9a2054563548a8200dbf46c1f5ae
Paulo H. Brito de Santanta <paulo.santana@southsystem.com.br>
2022-02-10 17:58:50 -0300

    adding new menus to side bar

--- src/app/view/movie-nav-bar/movie-nav-bar.component.ts
+++ src/app/view/movie-nav-bar/movie-nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { delay } from 'rxjs';
 import { FirebaseService } from 'src/app/service/firebase.service';
 import { MoviesApiService } from 'src/app/service/movies-api.service';
 
@@ -10,15 +11,36 @@ import { MoviesApiService } from 'src/app/service/movies-api.service';
 })
 export class MovieNavBarComponent implements OnInit {
   @Output() isLogout = new EventEmitter<void>();
+
+  genreslist: any;
+
   constructor(
     private apiService: MoviesApiService,
     private route: ActivatedRoute,
     public firebaseService: FirebaseService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getGenres();
+  }
   logout() {
     this.firebaseService.logout();
     this.isLogout.emit();
   }
+
+  getGenres() {
+    this.apiService
+      .getGenres()
+      .pipe(delay(1000))
+      .subscribe((res: any) => {
+        this.genreslist = res.genres;
+      });
+  }
+
+  // getMovieById(id: number): void {
+  //   this.apiService.getMovieDetails(id).subscribe((res: any) => {
+  //     this.genres = res['genres'];
+  //     this.details = res;
+  //   });
+  // }
 }
