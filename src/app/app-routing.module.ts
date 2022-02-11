f5a9372cc4b92fafecfd35b01104b38ef1f5acd3
Paulo H. Brito de Santanta <paulo.santana@southsystem.com.br>
2022-02-11 18:07:45 -0300

    update on routes and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GenresListComponent } from './view/genres-list/genres-list.component';
-import { GenresComponent } from './view/genres/genres.component';
+import { GenresComponent } from './components/genres/genres.component';
 import { HomeComponent } from './view/home/home.component';
-import { MovieSearchComponent } from './view/movie-search/movie-search.component';
+import { MovieSearchComponent } from './components/movie-search/movie-search.component';
 import { MovieTopRatedComponent } from './view/movie-top-rated/movie-top-rated.component';
 import { MovieTrendingComponent } from './view/movie-trending/movie-trending.component';
 import { MovieUpcomingComponent } from './view/movie-upcoming/movie-upcoming.component';
@@ -20,8 +20,7 @@ const routes: Routes = [
   { path: 'tvTrending/:page', component: TvTrendingComponent },
   { path: 'genres', component: GenresComponent },
   { path: 'genres/:id/:name', component: GenresListComponent },
-  { path: 'search', component: MovieSearchComponent }
-
+  { path: 'search', component: MovieSearchComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './view/home/home.component';
-import { MovieNavBarComponent } from './view/movie-nav-bar/movie-nav-bar.component';
-import { MovieSearchComponent } from './view/movie-search/movie-search.component';
+import { MovieNavBarComponent } from './components/movie-nav-bar/movie-nav-bar.component';
+import { MovieSearchComponent } from './components/movie-search/movie-search.component';
 import { MovieTrendingComponent } from './view/movie-trending/movie-trending.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
@@ -19,7 +19,13 @@ import { MovieUpcomingComponent } from './view/movie-upcoming/movie-upcoming.com
 import { TvTrendingComponent } from './view/tv-trending/tv-trending.component';
 import { TvTopComponent } from './view/tv-top/tv-top.component';
 import { GenresListComponent } from './view/genres-list/genres-list.component';
-import { GenresComponent } from './view/genres/genres.component';
+import { GenresComponent } from './components/genres/genres.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieFavoriteComponent } from './view/movie-favorite/movie-favorite.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,6 +39,11 @@ import { GenresComponent } from './view/genres/genres.component';
     TvTopComponent,
     GenresListComponent,
     GenresComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent,
+    MovieDetailsComponent,
+    MovieFavoriteComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,6 +53,7 @@ import { GenresComponent } from './view/genres/genres.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     FormsModule,
+    AngularFireDatabaseModule,
     ReactiveFormsModule,
   ],
   providers: [FirebaseService],
