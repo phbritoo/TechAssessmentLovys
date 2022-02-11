d5301c4f090a82ed6ae658515aa8c3ebc6ff9efa
Paulo H. Brito de Santanta <paulo.santana@southsystem.com.br>
2022-02-11 17:56:32 -0300

    updating the interfaces

--- src/app/modules/details.model.ts
+++ src/app/modules/details.model.ts
@@ -1,21 +1,10 @@
 export class Details {
-  id: number;
-  title: string;
-  overview: string;
-  budget: number;
-  vote_average: number;
-
-  constructor(
-    id: number,
-    title: string,
-    overview: string,
-    budget: number,
-    vote_average: number
-  ) {
-    this.id = id;
-    this.title = title;
-    this.overview = overview;
-    this.budget = budget;
-    this.vote_average = vote_average;
-  }
+  id?: string | null;
+  title?: string;
+  overview?: string;
+  vote_average?: string;
+  release_date?: string;
+  runtime?: string;
+  budget?: string;
+  revenue?: string;
 }
